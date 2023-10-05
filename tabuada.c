2a9740ab0366ffb5ea5414978a72b92476eb2091
helenojose <115164170+helenojose@users.noreply.github.com>
2023-10-05 10:31:01 -0300

    Create tabuada.c

---
+++ tabuada.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	
+	int tabuada, i, d, s;
+	
+	
+		printf("Escolha qual tabuada vc quer consultar.\n\n");
+	
+		printf("Digite a tabuada que deseja.\n");
+		scanf("%d",&tabuada);
+			
+			printf("Soma\n");
+			for(i=1; i<=10; i++){
+				printf("%d+%d = %d\n", i, tabuada, i + tabuada);
+			}
+				printf("Subtracao\n");
+			for(i=0;i<=10;i++){
+				s = tabuada+i;
+				printf("%d-%d = %d\n", s, tabuada, s - tabuada);
+			}
+				printf("Multiplicacao\n");
+			for (i=1; i<=10; i++){
+				printf("%dx%d = %d\n", i, tabuada, i * tabuada);
+			}
+				printf("Divisao\n");
+			for(i=1; i<=10; i++){
+				d = tabuada*i;
+				printf("%d/%d = %d\n", d, tabuada, d / tabuada);
+			}
+		
+		
+		
+	
+	
+		system ("pause");
+		return 0;
+}
